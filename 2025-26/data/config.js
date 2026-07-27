5e9a948e550551b34297adf4dbe83cb27ac1de48
OgnjenGrk <155249583+OgnjenGrk@users.noreply.github.com>
2026-07-27 16:00:57 +0200

    Add files via upload

---
+++ 2025-26/data/config.js
@@ -0,0 +1,1 @@
+const SEASON_CONFIG = {"id": "2025-26", "label": "Сезона 2025/26", "icon": "🏆", "dateFrom": "1. 9. 2025.", "dateTo": "31. 5. 2026."};
---
+++ leto-2026/data/config.js
@@ -0,0 +1,1 @@
+const SEASON_CONFIG = {"id": "leto-2026", "label": "Летња развојна лига 2026", "icon": "☀️", "dateFrom": "1. 6. 2026.", "dateTo": "31. 8. 2026."};
